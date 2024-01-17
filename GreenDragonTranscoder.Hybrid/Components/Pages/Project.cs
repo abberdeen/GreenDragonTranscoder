a8c9e6bc9fc021eb3a49c3a45340252bce1c3e19
abberdeen <abdurahim.khudoyberdiev@gmail.com>
2024-01-18 02:34:08 +0500

    Fix

---
+++ GreenDragonTranscoder.Hybrid/Components/Pages/Project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDragonTranscoder.Hybrid.Components.Pages
+{
+    public class Project
+    {
+        public string Name { get; set; }
+        public Project(string name)
+        {
+            Name = name;
+        }
+    }
+
+}
---
+++ GreenDragonTranscoder.Hybrid/Components/Pages/ProjectOutput.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDragonTranscoder.Hybrid.Components.Pages
+{
+    public class ProjectOutput
+    {
+        public string Codec { get; set; }
+        public string FPS { get; set; }
+        public double Progress { get; set; }
+        public bool Started { get; set; }
+
+        public ProjectOutput(string codec, string fps, double progress, bool started)
+        {
+            Codec = codec;
+            FPS = fps;
+            Progress = progress;
+            Started = started;
+        }
+    }
+}
