e9e2fe038adec95bb9d9aa25895b52b270ca2712
abberdeen <abdurahim.khudoyberdiev@gmail.com>
2024-01-17 22:37:42 +0500

    Add project files.

---
+++ GreenDragonTranscoder.Hybrid/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GreenDragonTranscoder.Hybrid
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ GreenDragonTranscoder.Hybrid/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GreenDragonTranscoder.Hybrid
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
